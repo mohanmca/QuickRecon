590c1e79ba3ed7d6760a5aacaebd23cd18dac100
Mani challenge <tv.mani@gmail.com>
2015-03-12 08:00:10 +0800

    quick recon

---
+++ QuickRecon/DBHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SQLite;
+
+namespace QuickRecon
+{
+    public class DBHelper
+    {
+        public static DbConnection GetConnection(string appName)
+        {
+            ConnectionStringSettings connectionStringSettings =
+                 ConfigurationManager.ConnectionStrings[appName];
+
+            DbProviderFactory factory =
+              DbProviderFactories.GetFactory(connectionStringSettings.ProviderName);
+
+            DbConnection connection = factory.CreateConnection();
+
+            if (connectionStringSettings.ProviderName == "SQLite Data Provider (Entity Framework 6)")
+            {
+                SQLiteConnection.CreateFile(connectionStringSettings.ConnectionString);
+            }
+
+            connection.ConnectionString = connectionStringSettings.ConnectionString;
+
+            connection.Open();
+
+            return connection;
+        }
+        public static void Close(DbConnection connection)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+    }
+}
